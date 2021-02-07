8b32bd86f92e295bdb278aa638ac16079d0147a3
Abobakr <abobakryousre@gmail.com>
2021-02-07 23:50:49 +0200

    add character, level screens. and  buttons sound

--- js/navigator.js
+++ js/navigator.js
@@ -3,6 +3,8 @@ let loginScene = document.getElementsByClassName("login")[0];
 let welcomeScene = document.getElementsByClassName("welcome")[0];
 let howToPlayScene = document.getElementsByClassName("how-to-play")[0];
 let aboutScene = document.getElementsByClassName("about")[0];
+let charactersScene = document.getElementsByClassName("characters")[0];
+let levelScene = document.getElementsByClassName("levels")[0];
 
 let username = document.getElementById("username");
 let loginBtn = document.getElementById("loginBtn");
@@ -12,19 +14,37 @@ let aboutBtn = document.getElementById("about");
 let exitBtn = document.getElementById("exit");
 let backFromHowToPlayBtn = document.getElementById("backFromHowToPlay");
 let backFromAboutUsBtn = document.getElementById("backFromAboutUs");
+let backFromCharacters = document.getElementById("backFromCharacters");
+let backFromLevel = document.getElementById("backFromLevel");
+let characterOne = document.getElementById("characterOne");
+let characterTwo = document.getElementById("characterTwo");
+let characterThree = document.getElementById("characterThree");
 
-loginBtn.addEventListener("click", checkUsername); // why when i called the checkUsername Function with () not working
-playBtn.addEventListener("click", startGame);
+let buttonSound = document.createElement("audio");
+buttonSound.src = "../sound/bubble_01.ogg";
+
+loginBtn.addEventListener("click", checkUsername);
+playBtn.addEventListener("click", choseCharacters);
 howToPlayBtn.addEventListener("click", howToPlay);
 aboutBtn.addEventListener("click", displayInforamtion);
 exitBtn.addEventListener("click", loadLoginScene);
+
+characterOne.addEventListener("click", loadLevelScene);
+characterTwo.addEventListener("click", loadLevelScene);
+characterThree.addEventListener("click", loadLevelScene);
+
+
 backFromHowToPlayBtn.addEventListener("click", loadWelcomeScene);
 backFromAboutUsBtn.addEventListener("click", loadWelcomeScene);
+backFromCharacters.addEventListener("click", loadWelcomeScene);
+backFromLevel.addEventListener("click", loadCharacterScene);
+
+
 
 
 function checkUsername(event){
-
-    if(username.value.match(/\b([a-zÀ-ÿ][-,a-z. ']+[ ]*)+/)){
+    buttonSound.play();
+    if(username.value){
         loadWelcomeScene();
     }else{
         alert("invaled username");
@@ -32,24 +52,46 @@ function checkUsername(event){
     }
 }
 function loadWelcomeScene(){
+    buttonSound.play();
     loginScene.setAttribute("style", "display: none");
     howToPlayScene.setAttribute("style", "display: none");
     aboutScene.setAttribute("style", "display: none");
+    charactersScene.setAttribute("style", "display: none");
     welcomeScene.setAttribute("style", "display: block");
 }
-function loadLoginScene(event){
+function loadLoginScene(){
+    buttonSound.play();
+
     welcomeScene.setAttribute("style", "display: none");
     loginScene.setAttribute("style", "display: block");
 }
-function startGame(event){
+function choseCharacters(){
+    buttonSound.play();
+
+    welcomeScene.setAttribute("Style", "display: none");
+    charactersScene.setAttribute("style", "display: block");
 
 }
-function howToPlay(event){
+function howToPlay(){
+    buttonSound.play();
+
     welcomeScene.setAttribute("style", "display: none");
     howToPlayScene.setAttribute("style", "display: block");
 
 }
-function displayInforamtion(event){
+function displayInforamtion(){
+    buttonSound.play();
     welcomeScene.setAttribute("style", "display: none");
     aboutScene.setAttribute("style", "display: block");
+}
+function loadCharacterScene(){
+    buttonSound.play();
+
+    levelScene.setAttribute("style", "display: none");
+    charactersScene.setAttribute("style", "display: block");
+}
+function loadLevelScene(){
+    buttonSound.play();
+    charactersScene.setAttribute("style", "display: none");
+    levelScene.setAttribute("style", "display: block");
 }
