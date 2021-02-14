957745451ae9d4c9e0927b8319a501fcbd1b837d
hosam94 <houssamali94@gmail.com>
2021-02-14 21:24:00 +0200

    Gameover screens

---
+++ js/UpAndDownDivs.js
@@ -0,0 +1,85 @@
+
+document.onreadystatechange =  () => {
+
+    if (document.readyState === 'complete') {
+
+        loaderOut();
+       
+
+    }
+
+};
+
+let loaderOut = () => {
+
+     load.classList.add("fadeOutUp");
+
+};
+
+let displayGameOver = () => {
+    gameOverDiv.style.display = "block";
+    gameOverDiv.style.zIndex = 999;
+    gameOverDiv.classList.remove("bounceOutUp");
+    gameOverDiv.classList.add("bounceInDown");
+    GOSound.play();
+    fishPlayer.style.display = "none";
+    board.style.display = "none";
+
+};
+
+let gameOverUp = () => {
+    gameOverDiv.classList.remove("bounceInDown");
+    gameOverDiv.classList.add("bounceOutUp");
+    fishPlayer.style.display = "block";
+    board.style.display = "flex";
+    setTimeout(function () {
+
+        gameOverDiv.style.display = "none";
+        gameOverDiv.style.zIndex = 0;
+    }, 1000)
+
+};
+
+let displayCong = () => {
+    congDiv.style.display = "block";
+    congDiv.style.zIndex = 999;
+    congDiv.classList.remove("bounceOutUp");
+    congDiv.classList.add("bounceInDown");
+    congSound.play();
+    fishPlayer.style.display = "none";
+    board.style.display = "none";
+
+};
+
+let congUp = () => {
+    congDiv.classList.remove("bounceInDown");
+    congDiv.classList.add("bounceOutUp");
+    fishPlayer.style.display = "block";
+    board.style.display = "block";
+    setTimeout( () => {
+        congDiv.style.display = "none";
+        congDiv.style.zIndex = 0;
+    }, 1000)
+
+};
+
+let selectUp = () => {
+    fade(levelScene);
+    fade(staticBG);
+    // levelScene.style.display = "none" ;
+    // staticBG.style.display = "none" ;
+};
+
+let viewBadges = () => {
+    badgeDiv.style.display = "block" ; 
+    badgeDiv.classList.add("bounceInLeft") ;
+    badgeDiv.classList.remove("bounceOutLeft") ; 
+};
+
+let hideBadges = () => {
+    badgeDiv.classList.add("bounceOutLeft") ;
+    badgeDiv.classList.remove("bounceInLeft") ; 
+    badgeDiv.style.display = "none" ; 
+};
+
+
---
+++ js/specialFish.js
@@ -0,0 +1,69 @@
+
+// specialFish = new EnemyFish(-100, window.innerHeight / 2, "specialFish.gif", 0.4, "right", "");
+// specialFish.createFish();
+
+let showSpecialFish = () => {
+
+    if ( specialChFalg == true && specialFlag === 1 &&  (score === 15 || score === 45 || score === 75) ) {
+        
+        specialChFalg = false ;  // score change
+        specialFlag = 0;           // detect event
+        specialFish = new EnemyFish(-100, window.innerHeight / 2, "specialFish.gif", 0.4, "right", "");
+        specialFish.createFish();
+        specialFish.speedX = 4;
+        specialFish.speedY = 0;
+
+            specialFishInterval = setInterval( () => {
+                
+                specialFish.moveFishes()
+                
+                detectCollisionBetweenPlayerAndSpecialFish();
+
+                if (specialFish.x > window.innerWidth + specialFish.width) {
+                    container.removeChild(specialFish.element);
+                    clearInterval(specialFishInterval);
+                    // specialFish = new EnemyFish(-100, window.innerHeight / 2, "specialFish.gif", 0.4, "right", "");
+                    // specialFish.createFish();
+                    specialFlag = 1;
+                }
+                
+            }, 100);
+           
+        
+
+    }
+};
+
+
+let detectCollisionBetweenPlayerAndSpecialFish = () => {
+
+
+    if (specialFish.width >= parseInt(fishPlayer.style.left) - specialFish.x
+        && parseInt(fishPlayer.style.top) >= specialFish.y - fishPlayer.height
+        && parseInt(fishPlayer.style.top) <= specialFish.y + specialFish.height) {
+
+        container.removeChild(specialFish.element);
+
+        for (let i = 0; i < fishEnemiesList.length; i++) {
+
+            fishEnemiesList[i].height = INIT_FISH_HEIGHT / 2 ;
+            fishEnemiesList[i].width = INIT_FISH_WIDTH / 2;
+            fishEnemiesList[i].element.height = INIT_FISH_HEIGHT / 2 ;
+            fishEnemiesList[i].element.width = INIT_FISH_WIDTH / 2 ;
+
+        }
+
+        clearInterval(specialFishInterval);
+        specialFlag = 1;
+        // specialFish = new EnemyFish(-100, window.innerHeight / 2, "specialFish.gif", 0.4, "right", "");
+        // specialFish.speedX = 4;
+        // specialFish.speedY = 0;
+        // specialFish.createFish();
+        return;
+
+    }
+   
+    
+   
+
+};
